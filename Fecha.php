20872b270bac97aa1330ccab4066f0425044b74d
NahuelTello <matias.tello@est.fi.uncoma.edu.ar>
2022-03-27 01:03:06 -0300

    Fecha

--- Fecha.php
+++ Fecha.php
@@ -97,10 +97,40 @@ class Fecha{
         return $fecha_abr;
     }
 
+
+    private function incrementar_dia_limite(){
+        $diaFebrero = $this->getDiaFebrero();
+        $countDias = 0;
+        if
+        (($this->getDia() >= 30) && (($this->getMes() == 1) || ($this->getMes() == 4) || ($this->getMes() == 6) || ($this->getMes() == 9) || ($this->getMes() == 11))) {
+            while ($countDias >= 30) {
+                $countDias ++;
+            }
+        } elseif (($this->getDia() >= 31) && (($this->getMes() == 3) || ($this->getMes() == 5) || ($this->getMes() == 7) || ($this->getMes() == 8) || ($this->getMes() == 10) || ($this->getMes() == 12))) {
+            while ($countDias >= 31) {
+                $countDias++;
+            }
+        } elseif (($this->getDia() == $diaFebrero) && ($this->getMes() == 2)) {
+            while ($countDias >= $diaFebrero) {
+                $countDias++;
+            }
+        }
+    }
+
     public function incrementa_un_dia()
     {
-        //$coleccionMeses = ["enero", "febrero","marzo","abril","mayo","junio", "julio", "agosto", "septiembre", "obctubre", "noviembre", "diciembre"];
-
+        $diaFebrero = $this->getDiaFebrero();
+        if (($this->getDia() >= 30) && ( ($this->getMes() == 1 ) || ( $this->getMes() == 4 ) || ($this->getMes() == 6) || ($this->getMes() == 9) || ($this->getMes() == 11))) {
+            $this->incrementar_dia_limite();
+        } elseif (($this->getDia() >= 31) && (($this->getMes() == 3) || ($this->getMes() == 5) || ($this->getMes() == 7) || ($this->getMes() == 8) || ($this->getMes() == 10) || ($this->getMes() == 12))) {
+            $this->incrementar_dia_limite();
+        } elseif ( ($this->getDia() == $diaFebrero) && ($this->getMes() == 2) ) {
+            $this->incrementar_dia_limite();
+        } else {
+            $this->setDia($this->getDia() + 1);
+        }
+        $nuevaFecha = "{$this->getDia()} de {$this->getMes()} de {$this->getAnio()}";
+        return $nuevaFecha;
     }
 
     //incrementa una x cantidad de dias y retorna una nueva fecha
@@ -108,8 +138,11 @@ class Fecha{
      * @param number
      * @return String
      */
-    public function incrementar($fecha, $cantDias)
+    public function incrementar($cantDias)
     {
+        for ($i=1; $i <$cantDias ; $i++) { 
+            $this->incrementa_un_dia();
+        }
     }
 
 }
